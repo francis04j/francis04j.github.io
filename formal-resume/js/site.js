d455e93b4b394d029dfd2f91c6e77e3d9a3cd7fd
francis04j <francis04j@gmail.com>
2020-02-23 01:39:33 +0000

    correct js error

--- formal-resume/js/site.js
+++ formal-resume/js/site.js
@@ -7,35 +7,3 @@ if ("serviceWorker" in navigator) {
         });
     });
 }
-
-$(function () {
-    $(".scrollto").on("click", function (e) {
-        var target = this.hash;
-        e.preventDefault();
-        $("body").scrollTo(target, 800, { offset: -60, "axis": "y" });
-    });
-
-    $(window).on("scroll resize load", function () {
-        $("nav").removeClass("navbar-fixed-top");
-        $(".navbar-header, .navbar-right").addClass("hidden");
-
-        var scrollTop = $(this).scrollTop();
-        var topDistance = $("nav").offset().top;
-
-        if (topDistance > scrollTop) {
-            $("nav").removeClass("navbar-fixed-top");
-            $(".navbar-header, .navbar-right").addClass("hidden");
-        }
-        else {
-            $("nav").addClass("navbar-fixed-top");
-            $(".navbar-header, .navbar-right").removeClass("hidden");
-        }
-    });
-
-    $(".chart").easyPieChart({
-        barColor: "#008CBA",
-        trackColor: "#E8E8E8",
-        scaleColor: false,
-        lineWidth: 10
-    });
-});
