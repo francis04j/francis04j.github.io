fef66c3aed5e003733d253ac292bf215b45daca6
francis04j <francis04j@gmail.com>
2020-05-07 13:44:43 +0100

    Updates

--- src/components/container.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import containerStyles from "./container.module.css"
-
-export default ({ children }) => (
-  <section className={`container ${containerStyles.container}`}>{children}</section>
-)
