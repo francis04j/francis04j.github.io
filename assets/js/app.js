2b45efc368c507fcd4221ccc528bfff69527463c
Francis Adediran <francis.adediran@iongroup.com>
2024-03-28 17:23:50 +0000

    added ip filtering

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,13 @@ $(function() {
 	// Get the messages div.
 	var formMessages = $('#form-messages');
 
+	$.getJSON("https://api.ipify.org?format=json",
+            function (data) {
+ 
+                // Displayin IP address on screen
+                $("#ip").val(data.ip);
+            })
+
 	// Set up an event listener for the contact form.
 	$(form).submit(function(e) {
 		// Stop the browser from submitting the form.
@@ -18,25 +25,32 @@ $(function() {
 		var postMsg = {
 			"name": $('#name').val(),
 			"email": $('#email').val(),
-			"message": $('#message').val()
-		}	
-		apigClient.conversationsPost({}, postMsg, {}).then(
-			function(response){
-				// Make sure that the formMessages div has the 'success' class.
-				$(formMessages).removeClass('error');
-				$(formMessages).addClass('success');
+			"message": $('#message').val(),
+			"ip": $('#ip').val(),
+		}
+		
+		if($('#ip').val()!== ''){
+			apigClient.conversationsPost({}, postMsg, {}).then(
+				function(response){
+					// Make sure that the formMessages div has the 'success' class.
+					$(formMessages).removeClass('error');
+					$(formMessages).addClass('success');
 
-				// Set the message text.
-				$(formMessages).text("Message sent.");
-					
+					// Set the message text.
+					$(formMessages).text("Message sent.");
+						
 
-				// Clear the form.
-				$('#name').val('');
-				$('#email').val('');				
-				$('#message').val('');
+					// Clear the form.
+					$('#name').val('');
+					$('#email').val('');				
+					$('#message').val('');
+					$('#ip').val('')
 
-			}
-		)
+				}
+			)
+		}else{
+			$(formMessages).text("We received your inital message. We will be in touch shortly");
+		}
 		/*.fail(function(data) {
 			// Make sure that the formMessages div has the 'error' class.
 			$(formMessages).removeClass('success');
